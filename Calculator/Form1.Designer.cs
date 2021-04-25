139d75c09a5401f687737e4cf3b720407d13b5d4
SalageanDragos <salagean.dragos@gmail.com>
2021-04-25 22:01:56 +0300

    Version 1.0
    The first version of this program, calculates the optimal time for vertical and horizontal movements.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MassH_Text = new System.Windows.Forms.TextBox();
+            this.TorqueH_Text = new System.Windows.Forms.TextBox();
+            this.Distance_Label = new System.Windows.Forms.Label();
+            this.Torque_Label = new System.Windows.Forms.Label();
+            this.RPM_Label = new System.Windows.Forms.Label();
+            this.RPMH_Text = new System.Windows.Forms.TextBox();
+            this.CalculateHoriz_Button = new System.Windows.Forms.Button();
+            this.Mass_Label = new System.Windows.Forms.Label();
+            this.RadiusH_Text = new System.Windows.Forms.TextBox();
+            this.DistanceH_Text = new System.Windows.Forms.TextBox();
+            this.Radius_Label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Min_Label = new System.Windows.Forms.Label();
+            this.CalculateVeri_Button = new System.Windows.Forms.Button();
+            this.label27 = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MassH_Text
+            // 
+            this.MassH_Text.Location = new System.Drawing.Point(1441, 627);
+            this.MassH_Text.Name = "MassH_Text";
+            this.MassH_Text.Size = new System.Drawing.Size(100, 20);
+            this.MassH_Text.TabIndex = 0;
+            // 
+            // TorqueH_Text
+            // 
+            this.TorqueH_Text.Location = new System.Drawing.Point(1441, 653);
+            this.TorqueH_Text.Name = "TorqueH_Text";
+            this.TorqueH_Text.Size = new System.Drawing.Size(100, 20);
+            this.TorqueH_Text.TabIndex = 0;
+            // 
+            // Distance_Label
+            // 
+            this.Distance_Label.AutoSize = true;
+            this.Distance_Label.Location = new System.Drawing.Point(1386, 578);
+            this.Distance_Label.Name = "Distance_Label";
+            this.Distance_Label.Size = new System.Drawing.Size(49, 13);
+            this.Distance_Label.TabIndex = 1;
+            this.Distance_Label.Text = "Distance";
+            // 
+            // Torque_Label
+            // 
+            this.Torque_Label.AutoSize = true;
+            this.Torque_Label.Location = new System.Drawing.Point(1357, 656);
+            this.Torque_Label.Name = "Torque_Label";
+            this.Torque_Label.Size = new System.Drawing.Size(78, 13);
+            this.Torque_Label.TabIndex = 2;
+            this.Torque_Label.Text = "Motor\'s Torque";
+            // 
+            // RPM_Label
+            // 
+            this.RPM_Label.AutoSize = true;
+            this.RPM_Label.Location = new System.Drawing.Point(1367, 682);
+            this.RPM_Label.Name = "RPM_Label";
+            this.RPM_Label.Size = new System.Drawing.Size(68, 13);
+            this.RPM_Label.TabIndex = 3;
+            this.RPM_Label.Text = "Motor\'s RPM";
+            // 
+            // RPMH_Text
+            // 
+            this.RPMH_Text.Location = new System.Drawing.Point(1441, 679);
+            this.RPMH_Text.Name = "RPMH_Text";
+            this.RPMH_Text.Size = new System.Drawing.Size(100, 20);
+            this.RPMH_Text.TabIndex = 0;
+            // 
+            // CalculateHoriz_Button
+            // 
+            this.CalculateHoriz_Button.Location = new System.Drawing.Point(1474, 726);
+            this.CalculateHoriz_Button.Name = "CalculateHoriz_Button";
+            this.CalculateHoriz_Button.Size = new System.Drawing.Size(100, 44);
+            this.CalculateHoriz_Button.TabIndex = 4;
+            this.CalculateHoriz_Button.Text = "Horizontal Movement";
+            this.CalculateHoriz_Button.UseVisualStyleBackColor = true;
+            this.CalculateHoriz_Button.Click += new System.EventHandler(this.CalculateHorizontal_Button_Click);
+            // 
+            // Mass_Label
+            // 
+            this.Mass_Label.AutoSize = true;
+            this.Mass_Label.Location = new System.Drawing.Point(1403, 630);
+            this.Mass_Label.Name = "Mass_Label";
+            this.Mass_Label.Size = new System.Drawing.Size(32, 13);
+            this.Mass_Label.TabIndex = 1;
+            this.Mass_Label.Text = "Mass";
+            // 
+            // RadiusH_Text
+            // 
+            this.RadiusH_Text.Location = new System.Drawing.Point(1441, 601);
+            this.RadiusH_Text.Name = "RadiusH_Text";
+            this.RadiusH_Text.Size = new System.Drawing.Size(100, 20);
+            this.RadiusH_Text.TabIndex = 0;
+            this.RadiusH_Text.TextChanged += new System.EventHandler(this.Radius_Text_TextChanged);
+            // 
+            // DistanceH_Text
+            // 
+            this.DistanceH_Text.Location = new System.Drawing.Point(1441, 575);
+            this.DistanceH_Text.Name = "DistanceH_Text";
+            this.DistanceH_Text.Size = new System.Drawing.Size(100, 20);
+            this.DistanceH_Text.TabIndex = 0;
+            // 
+            // Radius_Label
+            // 
+            this.Radius_Label.AutoSize = true;
+            this.Radius_Label.Location = new System.Drawing.Point(1361, 604);
+            this.Radius_Label.Name = "Radius_Label";
+            this.Radius_Label.Size = new System.Drawing.Size(74, 13);
+            this.Radius_Label.TabIndex = 1;
+            this.Radius_Label.Text = "Wheel Radius";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1547, 578);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "m";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1547, 604);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "m";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1547, 634);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "kg";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1547, 656);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "N*m";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1547, 682);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = " ";
+            // 
+            // Min_Label
+            // 
+            this.Min_Label.AutoSize = true;
+            this.Min_Label.Location = new System.Drawing.Point(1357, 414);
+            this.Min_Label.Name = "Min_Label";
+            this.Min_Label.Size = new System.Drawing.Size(0, 13);
+            this.Min_Label.TabIndex = 5;
+            // 
+            // CalculateVeri_Button
+            // 
+            this.CalculateVeri_Button.Location = new System.Drawing.Point(1360, 726);
+            this.CalculateVeri_Button.Name = "CalculateVeri_Button";
+            this.CalculateVeri_Button.Size = new System.Drawing.Size(100, 44);
+            this.CalculateVeri_Button.TabIndex = 4;
+            this.CalculateVeri_Button.Text = "Vertical Movement";
+            this.CalculateVeri_Button.UseVisualStyleBackColor = true;
+            this.CalculateVeri_Button.Click += new System.EventHandler(this.CalculateVertical_Button_Click);
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label27.Location = new System.Drawing.Point(1367, 522);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(171, 24);
+            this.label27.TabIndex = 1;
+            this.label27.Text = "Linear Movement";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(1357, 463);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 13);
+            this.timeLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 812);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.Min_Label);
+            this.Controls.Add(this.CalculateVeri_Button);
+            this.Controls.Add(this.CalculateHoriz_Button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.RPM_Label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Torque_Label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Mass_Label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Radius_Label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.Distance_Label);
+            this.Controls.Add(this.RPMH_Text);
+            this.Controls.Add(this.DistanceH_Text);
+            this.Controls.Add(this.TorqueH_Text);
+            this.Controls.Add(this.RadiusH_Text);
+            this.Controls.Add(this.MassH_Text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MassH_Text;
+        private System.Windows.Forms.TextBox TorqueH_Text;
+        private System.Windows.Forms.Label Distance_Label;
+        private System.Windows.Forms.Label Torque_Label;
+        private System.Windows.Forms.Label RPM_Label;
+        private System.Windows.Forms.TextBox RPMH_Text;
+        private System.Windows.Forms.Button CalculateHoriz_Button;
+        private System.Windows.Forms.Label Mass_Label;
+        private System.Windows.Forms.TextBox RadiusH_Text;
+        private System.Windows.Forms.TextBox DistanceH_Text;
+        private System.Windows.Forms.Label Radius_Label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Min_Label;
+        private System.Windows.Forms.Button CalculateVeri_Button;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double RPM;
+        double Torque;
+        double Angle;
+        double Radius;
+        double Distance;
+        double Mass;
+        double Acceleration;
+        bool pressed;
+        double gearRatio;
+        double Velocity;
+        double Decceleration;
+        bool drawn = false;
+        bool horizontal = false;
+        bool vertical = false;
+        bool angular = false;
+        List<Label> Time=new List<Label>();
+        List<Label> GearRatio=new List<Label>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            pressed = false;
+        }
+
+        public double Convert(string a)
+        {
+            double x=0;
+            bool comma = false;
+            double prod = 1;
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == ',' || a[i] == '.')
+                    comma = true;
+                else
+                {
+                    if (comma == false)
+                    {
+                        x = x * 10 + a[i] - '0';
+                    }
+                    else
+                    {
+                        prod *= 10;
+                        x = x + (a[i] - '0')/prod;
+                    }
+                }
+            }
+            return x;
+        }
+        
+        private float CalculateTime(double Velocity,double Acceleration)
+        {
+            float time = 20;
+            if (horizontal)
+            {
+                if (Distance < (Velocity * Velocity) / (Acceleration * 2))
+                {
+                    time = (float)Math.Sqrt(Distance / Acceleration * 2);
+                }
+                else
+                {
+                    time = (float)Velocity / (float)Acceleration;
+                    time += (float)(Distance - (Velocity * Velocity) / (Acceleration * 2)) / (float)Velocity;
+                }
+            }
+            if (vertical)
+            {
+                Acceleration = Acceleration - Decceleration;
+                if(Acceleration>0)
+                    if (Distance < (Velocity * Velocity) / (Acceleration * 2))
+                    {
+                        time = (float)Math.Sqrt(Distance / Acceleration * 2);
+                    }
+                    else
+                    {
+                        time += (float)Velocity / (float)Acceleration;
+                        time += (float)(Distance - (Velocity * Velocity) / (Acceleration * 2)) / (float)Velocity;
+                    }
+                
+            }
+            return time;
+        }
+
+        private void AddLabels(PaintEventArgs e, int originx, int originy, int width, int height)
+        {
+            
+            for (int i = originy - height / 20; i >= originy - height; i -= (height / 20))
+            {
+                Label number = new Label();
+                float count = (float)(i - originy) / height * 2 * (-1);
+                number.Text = count.ToString();
+                number.AutoSize = true;
+                number.TextAlign = ContentAlignment.MiddleCenter;
+                number.Width = 30;
+                number.Location = new Point(originx - number.Width, i - 5);
+                Time.Add(number);
+                this.Controls.Add(Time[(int)(count*10+0.1)-1]);
+            }
+            int y = 0;
+            for (int i = originx + width / 20; i <= originx + width; i = i + (width / 20))
+            {
+                Label number = new Label();
+                float count = (float)(i - originx) / width * 5;
+                number.Text = count.ToString();
+                number.AutoSize = true;
+                number.TextAlign = ContentAlignment.MiddleCenter;
+                if (number.Text.Length != 1)
+                    number.Width = (number.Text.Length - 1) * 9;
+                else
+                    number.Width = 10;
+                number.Location = new Point(i - number.Width / 2, originy + 10);
+                GearRatio.Add(number);
+                this.Controls.Add(GearRatio[y++]);
+            }
+            drawn = true;
+        }
+
+        private void TurnToVertical()
+        {
+            float a = 0;
+            for (int i = 0; i < Time.Count; i++)
+            {
+                a = (float)(i + 1) * (float)0.5;
+                this.Controls.Remove(Time[i]);
+                Time[i].Text = a.ToString();
+                this.Controls.Add(Time[i]);
+            }
+        }
+
+
+        private void TurnToHorizontal()
+        {
+            float a = 0;
+            for (int i = 0; i < Time.Count; i++)
+            {
+                a = (float)(i + 1) * (float)0.1;
+                this.Controls.Remove(Time[i]);
+                Time[i].Text = a.ToString();
+                this.Controls.Add(Time[i]);
+            }
+        }
+
+        private void paintGrid(PaintEventArgs e,int originx,int originy,int width,int height)
+        {
+            Pen penAxis = new Pen(Color.Black, (float)1),penGrid= new Pen(Color.Gray, (float)0.1);
+            if (drawn == false)
+            {
+                AddLabels(e,originx,originy,width,height);
+            }
+            for (int i = originx + width / 20; i <= originx + width; i =i+ (width / 20))
+            {
+                e.Graphics.DrawLine(penGrid, i, originy, i, originy - height);
+                
+            }
+            for (int i = originy - height / 20; i >= originy - height; i -= (height / 20))
+            {
+                e.Graphics.DrawLine(penGrid, originx, i, originx + width, i);
+                
+            }
+            e.Graphics.DrawLine(penAxis, originx, originy, originx, originy - height);
+            e.Graphics.DrawLine(penAxis, originx, originy, originx + width, originy);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen=new Pen(Color.Red,(float)2);
+            int originx=30, originy=700, width=600, height=600;
+            paintGrid(e, originx, originy, width, height);
+
+            
+            Queue<PointF> puncte= new Queue<PointF>();
+            double minVal = 10;
+            double place = 0;
+            bool first = true;
+            if (pressed == true) {
+                for (gearRatio = 0.001; gearRatio < 5; gearRatio += 0.001)
+                {
+                    float currentTime = CalculateTime(Velocity * gearRatio, Acceleration / gearRatio);
+                    if (horizontal == true)
+                    {
+                        if (originy - currentTime * height / 2 > originy - height && currentTime > 0)
+                        {
+                            if (first == true)
+                            {
+                                puncte.Enqueue(new PointF((float)originx + (float)(gearRatio - 0.001) * width / 5, originy - height));
+                                puncte.Enqueue(new PointF((float)originx + (float)gearRatio * width / 5, originy - currentTime * height / 2));
+                                first = false;
+                            }
+                            else
+                            {
+                                puncte.Enqueue(new PointF((float)originx + (float)gearRatio * width / 5, originy - currentTime * height / 2));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (vertical)
+                        {
+                            if (originy - currentTime * height / 10 > originy - height && currentTime > 0)
+                            {
+                                if (first == true)
+                                {
+                                    puncte.Enqueue(new PointF((float)originx + (float)(gearRatio - 0.001) * width / 5, originy - height));
+                                    puncte.Enqueue(new PointF((float)originx + (float)gearRatio * width / 5, originy - currentTime * height / 10));
+                                    first = false;
+                                }
+                                else
+                                {
+                                    puncte.Enqueue(new PointF((float)originx + (float)gearRatio * width / 5, originy - currentTime * height / 10));
+                                }
+                            }
+                        }
+                    }
+
+                    if (minVal> currentTime)
+                    {
+                        minVal = currentTime;
+                        place = gearRatio;
+                    }
+                }
+                if(puncte.Count!=0)
+                e.Graphics.DrawCurve(pen,puncte.ToArray()); 
+
+                Min_Label.Text = "Best Gear Ratio for this is " + place.ToString();
+                timeLabel.Text = "Time needed with best Gear Ratio is " + minVal.ToString(); 
+            }
+
+        }
+
+        private void Radius_Text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateHorizontal_Button_Click(object sender, EventArgs e)
+        {
+            RPM = Convert(RPMH_Text.Text);
+            Mass = Convert(MassH_Text.Text);
+            Distance = Convert(DistanceH_Text.Text);
+            Torque = Convert(TorqueH_Text.Text);
+            Radius = Convert(RadiusH_Text.Text);
+            Acceleration = Torque / Mass / Radius;
+            Velocity = Radius * RPM * 3.14 / 30;
+            TurnToHorizontal();
+            pressed = true;
+            horizontal = true;
+            vertical = false;
+            angular = false;
+            this.Refresh();
+        }
+
+        private void CalculateVertical_Button_Click(object sender, EventArgs e)
+        {
+            RPM = Convert(RPMH_Text.Text);
+            Mass = Convert(MassH_Text.Text);
+            Distance = Convert(DistanceH_Text.Text);
+            Torque = Convert(TorqueH_Text.Text);
+            Radius = Convert(RadiusH_Text.Text);
+            Acceleration = Torque / Mass / Radius;
+            Decceleration = 9.806;
+            Velocity = Radius * RPM * 3.14 / 30;
+            pressed = true;
+            TurnToVertical();
+            horizontal = false;
+            vertical = true;
+            angular = false;
+            this.Refresh();
+        }
+    }
+}
